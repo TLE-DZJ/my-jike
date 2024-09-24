2bd4c4572be08e188ef6496f44de6e2d8a50f9a9
Deng <3102934816@qq.com>
2024-09-24 20:30:46 +0800

    Layout-根据路由路径反向高亮菜单

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -6,7 +6,7 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 const { Header, Sider } = Layout
 
@@ -36,6 +36,13 @@ const GeekLayout = () => {
     const path = route.key
     navigate(path)
   }
+
+  // 反向高亮
+  // 1 获取当前的路由路径
+  const location = useLocation()
+  console.log(location.pathname)
+  const selectedKey = location.pathname
+  
   return (
     <Layout>
       <Header className="header">
@@ -54,7 +61,7 @@ const GeekLayout = () => {
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={['1']}
+            selectedKeys={selectedKey}
             items={items}
             style={{ height: '100%', borderRight: 0 }}
             onClick={onMenuClick}>
