a8c388de7121cb52dd5006ba40760951918fbe5f
Deng <3102934816@qq.com>
2024-09-24 21:22:39 +0800

    退出登录实现

--- src/store/modules/user.jsx
+++ src/store/modules/user.jsx
@@ -1,7 +1,7 @@
 // 和用户相关的状态管理
 
 import { createSlice } from "@reduxjs/toolkit"
-import { request, setToken as _setToken, getToken } from "@/utils"
+import { request, setToken as _setToken, getToken, removeToken } from "@/utils"
 
 const userStore = createSlice({
   name: 'user',
@@ -25,13 +25,20 @@ const userStore = createSlice({
     },
     setUserInfo(state, action) {
       state.userInfo = action.payload
+    },
+
+    clearUserInfo (state) {
+      state.token = ''
+      state.userInfo = {}
+      removeToken()
     }
+
   }
 })
 
 // 解构出actionCreator
 
-const { setToken, setUserInfo } = userStore.actions
+const { setToken, setUserInfo, clearUserInfo } = userStore.actions
 
 // 获取reducer函数
 const userReducer = userStore.reducer
@@ -61,5 +68,5 @@ const fetchUserInfo = () => {
   }
 }
 
-export { fetchLogin, fetchUserInfo, setToken }
+export { fetchLogin, fetchUserInfo, setToken, clearUserInfo }
 export default userReducer
